77e063206484b00181c2253006030ac14de5cd15
moneshkannan <moneshkannan02@gmail.com>
2022-02-12 09:56:35 +0530

    setup done

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import {ThirdwebWeb3Provider} from '@3rdweb/hooks'
 
+const support_chain_ids = [4];
+const connectors = {
+  injected:{},
+}
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <ThirdwebWeb3Provider supportedChainIds={support_chain_ids} connectors={connectors}>
+      <Component {...pageProps} />
+    </ThirdwebWeb3Provider>
+  )
 }
 
 export default MyApp
