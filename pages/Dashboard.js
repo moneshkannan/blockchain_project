7eb15130b1d24e27cc5d4b26b27769eb6eba74b2
moneshkannan <moneshkannan02@gmail.com>
2022-02-16 20:43:16 +0530

    added sanityToken ThirdwebToken

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import styled from 'styled-components'
 import Main from '../components/Main'
 import Sidebar from '../components/Sidebar'
+import {ethers} from 'ethers'
+import { ThirdwebSDK } from '@3rdweb/sdk'
+import axios from 'axios'
 
+const sdk = new ThirdwebSDK(
+    new ethers.Wallet(
+        process.env.NEXT_PUBLIC_METAMASK_KEY,
+        ethers.getDefaultProvider(
+            'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161'
+        )
+    )
+)
 function Dashboard({address}) {
+  const [sanityTokens, setSanityTokens] = useState([])
+  const [thirdWebTokens, setThirdWebTokens] = useState([])
+
+  const getSanityAndThirdWebTokens = async () => {
+    const sanityTokensGetDataFromApi = await axios.get("https://dk0dg5q5.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D'coins'%5D%7B%0A%20%20name%2C%0A%20%20inrPrice%2C%0A%20%20contractAddress%2C%0A%20%20symbol%2C%0A%20%20logo%0A%7D")
+    console.log(sanityTokensGetDataFromApi['data']['result'])
+    const sanityTokens = sanityTokensGetDataFromApi['data']['result']
+    setSanityTokens(sanityTokens)
+    setThirdWebTokens(
+        sanityTokens.map(token => sdk.getTokenModule(token['contractAddress']))
+    )
+    // console.log(apiCoins)
+  }
+
+  useEffect(()=>{
+    getSanityAndThirdWebTokens();
+  },[])
+
+  console.log("sanity",sanityTokens)
+  console.log("thirdWebTokens",thirdWebTokens)
+
   return (
     <Wrapper>
       <Sidebar/>
