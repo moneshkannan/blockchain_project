0109a8623e7e3abe6b4cb148b375b4e85fea0557
moneshkannan <moneshkannan02@gmail.com>
2022-02-17 14:11:38 +0530

    added send/receive modal with style

--- components/modal/TransferModel.js
+++ components/modal/TransferModel.js
@@ -1,12 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
 
 const TransferModel = () => {
+    const [action, setAction] = useState('send')
+
+    const selectedStyle = {
+        color: '#3773f5'
+    }
+    const unSelectedStyle = {
+        border: '1px solid #282b2f'
+    }
   return (
-    <div>
-        <p style={{color:'black'}}>send</p>
-        <p style={{color:'black'}}>receive</p>
-    </div>
+    <Wrapper>
+        <Selector>
+            <Options 
+                style={action === 'send' ? selectedStyle : unSelectedStyle} 
+                onClick={() => setAction('send')}
+                >
+                <p>send</p>
+            </Options>
+            <Options 
+                style={action === 'receive' ? selectedStyle : unSelectedStyle} 
+                onClick={() => setAction('receive')}
+                >
+                <p>receive</p>
+            </Options>
+        </Selector>
+    </Wrapper>
   )
 }
 
 export default TransferModel
+
+const Wrapper = styled.div`
+    height: 35rem;
+    width: 27rem;
+    color: white;
+    border: 1px solid #282b2f;
+    display: flex;
+    flex-direction: column;
+`
+
+const Selector = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    height: 5rem;
+`
+
+const Options = styled.div`
+    height: 100%;
+    width: 100%;
+    display: grid;
+    place-items: center;
+    font-size: 1.2rem;
+    font-weight: 600;
+
+    &:hover {
+        cursor: pointer;
+        background-color: #111214;
+    }
+`
